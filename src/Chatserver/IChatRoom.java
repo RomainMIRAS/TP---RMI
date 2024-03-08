86c2b449e9e1d8afc406068bde377a0a9cd691d5
Louane <lou.lesur@orange.fr>
2024-03-08 10:42:41 +0100

    Participant done

--- src/Chatserver/IChatRoom.java
+++ src/Chatserver/IChatRoom.java
@@ -3,10 +3,14 @@ package Chatserver;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface IChatRoom extends Remote{
+public interface IChatRoom extends Remote {
 	String name() throws RemoteException;
+
 	void connect(IParticipant p) throws RemoteException;
+
 	void leave(IParticipant p) throws RemoteException;
+
 	String[] who() throws RemoteException;
+
 	void send(IParticipant p, String msg) throws RemoteException;
 }
--- src/Chatserver/Participant.java
+++ src/Chatserver/Participant.java
@@ -1,5 +1,22 @@
 package Chatserver;
 
 public class Participant implements IParticipant{
+	
+	String name;
+	
+	
+	public Participant(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String name() {
+		return this.name;
+	}
+
+	@Override
+	public void receive(String name, String msg) {
+		System.out.println("Participant "+name+": "+msg);
+	}
 
 }
