d3dace4fd7043f838eac26d873e1d0081cf8613b
Romain Miras <romain.miras@etu.univ-grenoble-alpes.fr>
2024-03-08 10:22:53 +0100

    pog

--- src/Chatserver/ChatRoom.java
+++ src/Chatserver/ChatRoom.java
@@ -2,12 +2,37 @@ package Chatserver;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 
 public class ChatRoom extends UnicastRemoteObject implements IChatRoom{
 
 	protected ChatRoom() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void connect() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void leave() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void say(String msgToSay) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> who() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/Chatserver/IChatRoom.java
+++ src/Chatserver/IChatRoom.java
@@ -1,5 +1,12 @@
 package Chatserver;
 
-public interface IChatRoom {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
+public interface IChatRoom extends Remote{
+	void connect()  throws RemoteException;
+	void leave()  throws RemoteException;
+	void say(String msgToSay)  throws RemoteException;
+	List<String> who()  throws RemoteException;
 }
